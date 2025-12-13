61db13d1026004ea2cc9d0c446a81f18ef978a6a
maihuy243 <maihuy243@gmail.com>
2025-12-13 16:05:33 +0700

    disable cache

--- src/context/react-query.ts
+++ src/context/react-query.ts
@@ -4,11 +4,11 @@ const GC_TIME_ROOT = 1000 * 60 * 10
 const STALE_TIME_ROOT = 1000 * 30
 
 // Auto refetch sau 10p
-export const GC_TIME_HOOKS = 1000 * 60 * 10
-// export const GC_TIME_HOOKS = 1000 * 5 // Off cache
+// export const GC_TIME_HOOKS = 1000 * 60 * 10
+export const GC_TIME_HOOKS = 0 // Off cache
 
 // Remove sau 10p nếu không có request get cache
-export const STALE_TIME_HOOKS = 1000 * 60 * 10
+export const STALE_TIME_HOOKS = 1000 * 60 * 1
 
 export const queryClient = new QueryClient({
   defaultOptions: {
